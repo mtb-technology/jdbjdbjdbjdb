8132e648b6e794701d7f851678706e6cd2485f25
Beaudinn Greve <beaudinngreve@gmail.com>
2025-12-19 18:21:48 +0100

    update

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -46,6 +46,25 @@ server {
         proxy_set_header X-Forwarded-Proto \$scheme;
     }
 
+    # Static assets - NO basic auth (needed for embed views to load JS/CSS)
+    location ^~ /assets/ {
+        auth_basic off;
+        proxy_pass http://127.0.0.1:5000;
+        proxy_http_version 1.1;
+        proxy_set_header Host \$host;
+        proxy_set_header X-Real-IP \$remote_addr;
+        proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
+        proxy_set_header X-Forwarded-Proto \$scheme;
+    }
+
+    # Favicon - NO basic auth
+    location = /favicon.ico {
+        auth_basic off;
+        proxy_pass http://127.0.0.1:5000;
+        proxy_http_version 1.1;
+        proxy_set_header Host \$host;
+    }
+
     # Automail API routes - NO basic auth (for embedded view)
     location ^~ /api/automail/ {
         auth_basic off;
